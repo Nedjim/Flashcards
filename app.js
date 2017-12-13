df622ad1f3b96a16deb753eca60d33f759ea4f53
Nedjim <nedjim.danimon@gmail.com>
2017-12-13 11:52:33 +0100

    Add redirection

--- app.js
+++ app.js
@@ -12,21 +12,19 @@ const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'];
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-    res.render('index')
-});
-
-app.get('/hello', (req, res) => {
-    res.render('hello', {name: req.cookies.username})
+    const name = req.cookies.username;
+    name ? res.render('index', { name }) : res.render('hello');
 });
 
 app.post('/hello', (req, res) => {
     res.cookie('username', req.body.username)
-    res.render('hello', {name: req.body.username})
+    res.redirect('/')
 });
 
-app.get('/cards', (req, res) => {
-    res.render('card', {prompt: 'Who is buried in Grant\'s tom?'})
-})
+app.get('/hello', (req, res) => {
+    const name = req.cookies.username;
+    name ? res.redirect('/') : res.render('hello');
+});
 
 app.listen(3000, ()=>{
     console.log('The application is running on localhost:3000')
