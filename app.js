03490b379fc544f32adfb2ef63c7142a7d7d31d9
Nedjim <nedjim.danimon@gmail.com>
2017-12-14 16:04:02 +0100

    Add middleware function

--- app.js
+++ app.js
@@ -7,13 +7,23 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false}))
 app.use(cookieParser());
 
+app.use((req, res, next) => {
+    console.log('First middleware');
+    const err = new Error('Ho non!!');
+    next(err);
+})
+
+app.use((req, res, next) => {
+    next();
+})
+
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'];
 
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
     const name = req.cookies.username;
-    name ? res.render('index') : res.render('hello');
+    name ? res.render('index') : res.render('signin');
 });
 
 app.post('/signin', (req, res) => {
@@ -30,6 +40,10 @@ app.post('/goodbye', (req, res) => {
     res.clearCookie('username')
     res.redirect('/signin')
 });
+
+app.get('/cards', (req, res) => {
+    res.render('card', {prompt: 'Hey hey'})
+})
 
 app.listen(3000, ()=>{
     console.log('The application is running on localhost:3000')
